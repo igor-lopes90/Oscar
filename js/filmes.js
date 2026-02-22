72d9977adaf667c672339d9013fe5953870f67c1
Igor Lopes <igor_lopes90@yahoo.com.br>
2026-02-21 22:48:55 -0300

    Update filmes.js

--- js/filmes.js
+++ js/filmes.js
@@ -109,7 +109,7 @@ function renderizarTabela() {
     "HBO Max": "#6a00ff",
     "Mubi": "#111111",
     "Cinema": "#ff8800",
-    "Stremio": "#954ab2",
+    "Stremio": "#ac00c6",
     "N/A": "#777777"
   };
 
